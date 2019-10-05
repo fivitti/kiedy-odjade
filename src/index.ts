8ecce380478464037292476b7f96608e0fff4db1
Slawomir Fivitti Figiel <fivitti@gmail.com>
2019-10-05 17:30:55 +0200

    Add dumb error catcher

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,9 @@ import "papercss/dist/paper.min.css";
 
 import App from "./app";
 
+
 const app = new App("content");
 app.init()
-.then(() => app.refresh())
-.then(() => app.render());
+    .then(() => app.refresh())
+    .then(() => app.render())
+    .catch((err) => console.error(err));
