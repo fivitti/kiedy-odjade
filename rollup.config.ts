a198fba8c87e6ad625d5d04bcb9be37c2793cc89
Slawomir Fivitti Figiel <fivitti@gmail.com>
2019-10-05 14:46:42 +0200

    Publish to github pages

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,22 +1,20 @@
-import resolve from 'rollup-plugin-node-resolve'
-import commonjs from 'rollup-plugin-commonjs'
-import sourceMaps from 'rollup-plugin-sourcemaps'
-import typescript from 'rollup-plugin-typescript2'
-import minify from 'rollup-plugin-babel-minify';
-import postcss from 'rollup-plugin-postcss';
 import cssnano from 'cssnano';
+import minify from 'rollup-plugin-babel-minify';
+import commonjs from 'rollup-plugin-commonjs';
 import copy from 'rollup-plugin-copy-glob';
-import gzipPlugin from 'rollup-plugin-gzip'
+import gzipPlugin from 'rollup-plugin-gzip';
+import resolve from 'rollup-plugin-node-resolve';
+import postcss from 'rollup-plugin-postcss';
+import sourceMaps from 'rollup-plugin-sourcemaps';
+import typescript from 'rollup-plugin-typescript2';
 
 const pkg = require('./package.json')
-
-const libraryName = 'ztm-gdansk'
 
 export default {
   input: `src/index.ts`,
   output: [
     //{ file: pkg.main, name: libraryName, format: 'amd', sourcemap: true },
-    { file: pkg.module, format: 'es', sourcemap: true },
+    { file: "dist/bundle.min.js", format: 'es', sourcemap: false },
   ],
   // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
   external: [],
