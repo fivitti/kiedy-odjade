2408b4ef721134e8f41ed7912d8ad5f3e50fac77
Slawek Figiel <fivitti@gmail.com>
2019-10-15 19:16:21 +0200

    Hosting app configuration

---
+++ app.js
@@ -0,0 +1,16 @@
+var host = process.env.HOST || '0.0.0.0';
+// Listen on a specific port via the PORT environment variable
+var port = process.env.PORT || 8080;
+  
+var cors_proxy = require('./cors-anywhere');
+cors_proxy.createServer({
+    originWhitelist: ["https://figiel.xyz", "https://fivitti.github.io"], // Allow all origins
+    requireHeader: ['origin', 'x-requested-with'],
+    removeHeaders: ['cookie', 'cookie2'],
+    corsMaxAge: 600,
+    setResponseHeaders: {
+        'cache-control': 'max-age=30',
+    }
+}).listen(port, host, function() {
+    console.log('Running CORS Anywhere on ' + host + ':' + port);
+});
